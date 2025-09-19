f375a0c05164f4ce53aef4f0dd2afe38b6244e21
John Louie Cleofas <164037152+JLCleofas@users.noreply.github.com>
2025-09-19 11:23:06 +0800

    Working logic but still no error handling

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,58 +1,31 @@
 from random import choice
-class Game():
-    def intro(self):
+
+class Game:
+    def __init__(self):
+        self.rounds = 0
+    
+    def start(self):
         print('--- Rock Paper Scissors Game ---')
         self.rounds = int(input('How many rounds would you like to play? '))
-
+    
     def play(self):
         for _ in range(self.rounds):
-            self.choice = input('Rock, paper, or scissors [r/p/s]? ')
-            computer_choice = ComputerChoice().generate_choice()
-            print(f'You: {self.choice}   |  Computer: {computer_choice}')
-            result = Result(self.choice, computer_choice)
-            print(result.result())
-
-class ComputerChoice():
-    def generate_choice(self):
-        return str(choice('rps'))
-
-class Result():
-    def __init__(self, user_choice, computer_choice):
-        self.user_choice = user_choice
-        self.computer_choice = computer_choice
+            self.user_choice = input('Rock, paper, or scissors [r/p/s]? ').strip().lower()
+            self.computer_choice = choice('rps')
+            print(f'You: {self.user_choice}   |  Computer: {self.computer_choice}')
+            result = self.decision(self.user_choice, self.computer_choice)
+            print(result)
     
-    def result(self):
-        if self.user_choice == 'r':
-            return Rock().rock_outcomes(self.computer_choice)
-        if self.user_choice == 'p':
-            return Paper().paper_outcomes(self.computer_choice)
-        if self.user_choice == 's':
-            return Scissors().scissors_outcomes(self.computer_choice)
-
-
-class Rock(Game):
-    def rock_outcomes(self, computer_choice):
-        if computer_choice == 'r':
+    def decision(self, user, computer):
+        if user == computer:
             return 'It was a tie.'
-        if computer_choice == 'p':
-            return 'Computer won.'
-        if computer_choice == 's':
-            return 'You won.'
+        user_vs_computer = (user, computer)
+        wins = [('r', 's'), ('p', 'r'), ('s', 'p')]
+        return 'You won.' if user_vs_computer in wins else 'Computer won.'
 
-class Paper(Game):
-    def paper_outcomes(self, computer_choice):
-        if computer_choice == 'r':
-            return 'You won.'
-        if computer_choice == 'p':
-            return 'It was a tie.'
-        if computer_choice == 's':
-            return 'Computer won.'
+    def run(self):
+        self.start()
+        self.play()
 
-class Scissors(Game):
-    def scissors_outcomes(self, computer_choice):
-        if computer_choice == 'r':
-            return 'Computer won.'
-        if computer_choice == 'p':
-            return 'You won.'
-        if computer_choice == 's':
-            return 'It was a tie.'
+game = Game()
+game.run()
